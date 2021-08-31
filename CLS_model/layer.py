b6517cf48deb18893fe4f5d59c8f703146407907
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-08-31 23:20:20 +0900

    Update layer.py

--- CLS_model/layer.py
+++ CLS_model/layer.py
@@ -1,7 +1,7 @@
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
-from models.embedding import TransformerEmbedding
+from CLS_model.embedding import TransformerEmbedding
 from util.gelu import GELU
 
 class gMLPBLOCK_CLS(nn.Module):
