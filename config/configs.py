15fa81f7f5044b6d1a4811eda85717a4bf0fb7b3
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-03 12:57:19 +0900

    Update configs.py

--- config/configs.py
+++ config/configs.py
@@ -19,11 +19,11 @@ parser.add_argument('--val_batch_size',type=int, default=256)
 parser.add_argument('--test_batch_size',type=int,default=2)
 parser.add_argument('--display_examples',type=int, default=1000)
 
-parser.add_argument('--model_dim', type=int, default=512)
+parser.add_argument('--model_dim', type=int, default=768)
 parser.add_argument('--key_dim',type=int, default = 512)
 parser.add_argument('--value_dim',type=int, default= 512)
-parser.add_argument('--hidden_dim', type=int, default=2048)
-parser.add_argument('--num_layers', type=int, default=12)
+parser.add_argument('--hidden_dim', type=int, default=1024)
+parser.add_argument('--num_layers', type=int, default=24)
 parser.add_argument('--num_heads', type=int, default=8)
 parser.add_argument('--drop_prob',type=float, default=0.1)
 
@@ -38,8 +38,8 @@ parser.add_argument('--clip',type=int, default=1)
 parser.add_argument('--weight_decay',type=float, default=0.01)
 parser.add_argument('--decay_epoch',type=int, default=10000)
 
-parser.add_argument('--max_len', type=int, default=64)
-parser.add_argument('--vocab_size',type=int, default=28996)
+parser.add_argument('--max_len', type=int, default=128)
+parser.add_argument('--vocab_size',type=int, default=50265)
 
 # tokenizer
 parser.add_argument('--language',type=str, default='en')
