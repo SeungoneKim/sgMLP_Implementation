918977c132264cdbff28793f87ae5110c9db1ce2
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-09-03 13:38:09 +0900

    adding horovod support

--- hovorod_ver/data/dataloader.py
+++ hovorod_ver/data/dataloader.py
@@ -1,5 +1,7 @@
+import hovorod.torch as hvd
 from torch.utils.data import DataLoader, Dataset
-from data.dataset import PretrainDataset, PretrainDataset_total
+from torch.utils.data.distributed import DistributedSampler
+from horovod_ver.data.dataset import PretrainDataset, PretrainDataset_total
 
 """
 book_train, book_val, book_test = get_Pretrain_dataloader(3,3,3,'en',128,'bookcorpus','plain_text','text',0.5,0.15,0.8,0.1,0.1)
@@ -13,16 +15,25 @@ def get_Pretrain_dataloader(train_batch_size, val_batch_size, test_batch_size,
                 dataset_name, dataset_type, category_type, next_sent_prob, masking_prob,
                 training_ratio, validation_ratio, test_ratio, percentage)
     
-    train_dataloader = DataLoader(dataset=dataset.getTrainData(),
+    train_dataset = dataset.getTrainData()
+    train_sampler = DistributedSampler(train_dataset, num_replicas=hvd.size(), rank=hvd.rank())
+    train_dataloader = DataLoader(dataset=train_dataset,
                             batch_size=train_batch_size,
+                            sampler=train_sampler,
                             shuffle=True)
-
-    val_dataloader = DataLoader(dataset=dataset.getValData(),
-                            batch_size=val_batch_size,
-                            shuffle=True)      
     
-    test_dataloader = DataLoader(dataset=dataset.getTestData(),
+    val_dataset = dataset.getValData()
+    val_sampler = DistributedSampler(val_dataset, num_replicas=hvd.size(), rank=hvd.rank())
+    val_dataloader = DataLoader(dataset=val_dataset,
+                            batch_size=val_batch_size,
+                            sampler=val_sampler,
+                            shuffle=True)  
+    
+    test_dataset = dataset.getTestData()
+    test_sampler = DistributedSampler(test_dataset, num_replicas=hvd.size(), rank=hvd.rank())
+    test_dataloader = DataLoader(dataset=test_dataset,
                             batch_size=test_batch_size,
+                            sampler=text_sampler,
                             shuffle=True)      
     
     return train_dataloader, val_dataloader, test_dataloader
